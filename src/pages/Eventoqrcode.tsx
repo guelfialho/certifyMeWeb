8c41f7829128f351a7e29d531b6636fa63d31ef8
Monique <900moni@gmail.com>
2025-07-03 21:27:57 -0300

    review: remoção comentários

--- src/pages/Eventoqrcode.tsx
+++ src/pages/Eventoqrcode.tsx
@@ -1,12 +1,9 @@
-//import { useParams, useNavigate } from "react-router-dom";
 import QRCode from "react-qr-code";
 import "../estilos/Eventoqrcode.css";
 
 export default function Eventoqrcode() {
-    //const { id } = useParams<{ id: string }>();
-    //const navigate = useNavigate();
 
-    const token = Math.random().toString(36).substring(2, 10); // exemplo simples
+    const token = Math.random().toString(36).substring(2, 10);
     const link = `${window.location.origin}/pages/certificado/${token}`;
 
     const imprimir = () => {
